2e4fa2b5e738c93e1fdb8a320d21f91e85cf0b9c
nobutakayamauchi <ultimaterisingson@gmail.com>
2026-03-06 03:00:00 +0900

    Initialize RTS Core Package

---
+++ rts_core/__init__.py
@@ -0,0 +1,9 @@
+"""
+RTS Core Package
+
+This module defines the structural core of RTS.
+Reconstructability is guaranteed at the structural level.
+"""
+
+CORE_VERSION = "0.1.0"
+CORE_MODE = "STRUCTURAL_RECONSTRUCTABILITY"
