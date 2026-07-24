7c13f6850d36a91a56b899c944fc257226571c8c
nobutakayamauchi <ultimaterisingson@gmail.com>
2026-07-25 07:33:28 +0900

    Add Skill regression evaluation core

---
+++ skill_regression/models.py
@@ -0,0 +1,10 @@
+from skill_regression.common import *  # noqa: F401,F403
+from skill_regression.evaluation import evaluate_dataset, fixture_outcome, subsequence
+from skill_regression.validators import (
+    validate_contract,
+    validate_dataset,
+    validate_fixture,
+    validate_result,
+    validate_rollback,
+    validate_snapshot,
+)
