3f24ebc3311d36ffab8888ed56bfd8d8c0cb193f
nobutakayamauchi <ultimaterisingson@gmail.com>
2026-08-08 03:26:39 +0900

    Add runtime debug gate package

---
+++ runtime_debug_gate/__init__.py
@@ -0,0 +1,5 @@
+"""RTS runtime debug classification gate."""
+
+from .core import DebugGateError, evaluate_debug_gate, validate_gate_result
+
+__all__ = ["DebugGateError", "evaluate_debug_gate", "validate_gate_result"]
