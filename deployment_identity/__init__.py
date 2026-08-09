46072b377df6f6be5fef1732ea78116e2d9ad59f
nobutakayamauchi <ultimaterisingson@gmail.com>
2026-08-09 18:53:17 +0900

    Export attested deployment authorization boundary

--- deployment_identity/__init__.py
+++ deployment_identity/__init__.py
@@ -1,15 +1,27 @@
 """Deployment identity proof boundary for RTS."""
 
+from .attestation import (
+    AttestationError,
+    compute_hmac_signature,
+    establish_attested_deployment_identity,
+    verify_attestation_quorum,
+)
 from .core import (
     DeploymentIdentityError,
     bind_runtime_observation,
     establish_deployment_identity,
+    fingerprint_expectation,
     fingerprint_observation,
 )
 
 __all__ = [
+    "AttestationError",
     "DeploymentIdentityError",
     "bind_runtime_observation",
+    "compute_hmac_signature",
+    "establish_attested_deployment_identity",
     "establish_deployment_identity",
+    "fingerprint_expectation",
     "fingerprint_observation",
+    "verify_attestation_quorum",
 ]
