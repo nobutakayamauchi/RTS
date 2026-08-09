143878e4cab03f3a208fcfae5d59f56f55a378ff
nobutakayamauchi <ultimaterisingson@gmail.com>
2026-08-09 19:27:11 +0900

    Export outcome closure API

--- deployment_identity/__init__.py
+++ deployment_identity/__init__.py
@@ -13,15 +13,29 @@ from .core import (
     fingerprint_expectation,
     fingerprint_observation,
 )
+from .outcome import (
+    OUTCOME_BOUND,
+    OUTCOME_NOT_BOUND,
+    OutcomeEvidenceError,
+    bind_outcome_evidence,
+    compute_outcome_signature,
+    fingerprint_outcome,
+)
 
 __all__ = [
     "AttestationError",
     "DeploymentIdentityError",
+    "OUTCOME_BOUND",
+    "OUTCOME_NOT_BOUND",
+    "OutcomeEvidenceError",
+    "bind_outcome_evidence",
     "bind_runtime_observation",
     "compute_hmac_signature",
+    "compute_outcome_signature",
     "establish_attested_deployment_identity",
     "establish_deployment_identity",
     "fingerprint_expectation",
     "fingerprint_observation",
+    "fingerprint_outcome",
     "verify_attestation_quorum",
 ]
