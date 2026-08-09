18bebd1d09aa88bccd4939af670aeff04509a0f9
nobutakayamauchi <ultimaterisingson@gmail.com>
2026-08-09 18:41:24 +0900

    Export runtime observation binding API

--- deployment_identity/__init__.py
+++ deployment_identity/__init__.py
@@ -1,9 +1,15 @@
 """Deployment identity proof boundary for RTS."""
 
-from .core import DeploymentIdentityError, establish_deployment_identity, fingerprint_observation
+from .core import (
+    DeploymentIdentityError,
+    bind_runtime_observation,
+    establish_deployment_identity,
+    fingerprint_observation,
+)
 
 __all__ = [
     "DeploymentIdentityError",
+    "bind_runtime_observation",
     "establish_deployment_identity",
     "fingerprint_observation",
 ]
