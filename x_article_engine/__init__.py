9c5d54ae4479cc355f59fde6b209e619bcc05218
nobutakayamauchi <ultimaterisingson@gmail.com>
2026-08-16 08:21:34 +0900

    Route public article engine through terminology gate

--- x_article_engine/__init__.py
+++ x_article_engine/__init__.py
@@ -1,9 +1,5 @@
-from .core import (
-    XArticleEngineError,
-    audit_draft,
-    build_generation_packet,
-    normalize_brief,
-)
+from .core import XArticleEngineError, normalize_brief
+from .terminology import audit_draft, build_generation_packet
 
 __all__ = [
     "XArticleEngineError",
