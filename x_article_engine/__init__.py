6f0be01f783981c550f3eab17b907d7d25d8fbf9
nobutakayamauchi <ultimaterisingson@gmail.com>
2026-08-16 08:56:23 +0900

    Expose v0.8 depth market engine

--- x_article_engine/__init__.py
+++ x_article_engine/__init__.py
@@ -1,5 +1,5 @@
 from .core import XArticleEngineError, normalize_brief
-from .longform_filter import audit_draft, build_generation_packet
+from .depth_market import audit_draft, build_generation_packet
 
 __all__ = [
     "XArticleEngineError",
