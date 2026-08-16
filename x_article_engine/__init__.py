7f063cf55f248c05edcb6b67d2eea39c2e02e809
nobutakayamauchi <ultimaterisingson@gmail.com>
2026-08-16 10:10:38 +0900

    Route X Article Engine root through consolidated v0.9

--- x_article_engine/__init__.py
+++ x_article_engine/__init__.py
@@ -1,5 +1,5 @@
 from .core import XArticleEngineError, normalize_brief
-from .meteor_v09_r5 import audit_draft, build_generation_packet
+from .v09 import audit_draft, build_generation_packet
 
 __all__ = [
     "XArticleEngineError",
