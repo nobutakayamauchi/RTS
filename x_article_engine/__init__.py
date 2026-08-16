9c1b85c3709f7a96f7f755ebadafd00975242a72
nobutakayamauchi <ultimaterisingson@gmail.com>
2026-08-16 10:19:22 +0900

    Route package root through hardened v0.9 semantic gate

--- x_article_engine/__init__.py
+++ x_article_engine/__init__.py
@@ -1,5 +1,5 @@
 from .core import XArticleEngineError, normalize_brief
-from .v09 import audit_draft, build_generation_packet
+from .v09_hardened import audit_draft, build_generation_packet
 
 __all__ = [
     "XArticleEngineError",
